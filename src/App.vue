11820c270a22e519eb5a10b1587cf977b5ffae6a
svetanguyen <matematicaforeber@gmail.com>
2021-05-30 14:14:06 +0700

    collect data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <Header :totalIncome="state.totalIncome" />    
-    <Form />
+    <Form @add-income="AddIncome" />
 </template>
 
 <script>
@@ -21,10 +21,25 @@ export default {
         return temp;
       })
     });
+
+    function AddIncome(data) {
+      let d = data.date.split('-');
+      let newD = new Date(d[0], d[1], d[2]);
+      state.income = [...state.income, {
+        id: Date.now(),
+        desc: data.desc,
+        value: +data.value,
+        date: newD.getTime()
+      }]
+
+      console.log(state.income)
+    }
+
     return {
       Header,
       Form,
-      state
+      state,
+      AddIncome
     }
   },
   components: {
